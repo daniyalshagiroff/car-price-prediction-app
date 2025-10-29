d84134db2fb74829ebe47028f86a3ed774e2b81b
Daniyal <daniyalshagiroff@gmail.com>
2025-10-29 14:02:45 +0400

    SQL queries added: get_best_cars, get_most_profit_cars

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
+from repo import get_best_cars, get_most_profit_cars, carbon_vs_no_carbon, interior_vs_no_interior
 
 app = FastAPI()
 
-@app.get("/best_cars")
-def get_best_cars():
-    return True
-
+@app.get("/home")
+def get_dashboard(amount: int = 5):
+    return get_best_cars(amount), get_most_profit_cars(amount), carbon_vs_no_carbon(amount), interior_vs_no_interior(amount)
--- backend/app/repo.py
+++ backend/app/repo.py
@@ -10,7 +10,24 @@ key = os.getenv("SUPABASE_KEY")
 supabase = create_client(url, key)
 
 def get_best_cars(amount):
-    result = supabase.rpc("get_best_cars", {"amount": 10}).execute()
-    print(result.data)
+    result = supabase.rpc("get_best_cars", {"amount": amount}).execute()
+    return result.data
 
-get_best_cars(10)
+def get_most_profit_cars(amount):
+    result = supabase.rpc("get_most_profit_cars", {"amount": amount}).execute()
+    return result.data
+
+def carbon_vs_no_carbon(amount):
+    result_carbon = supabase.rpc("get_best_cars", {"amount": amount, "carbon": True}).execute()
+    result_no_carbon = supabase.rpc("get_best_cars", {"amount": amount, "carbon": False}).execute()
+    return result_carbon.data, result_no_carbon.data
+
+def interior_vs_no_interior(amount):
+    result_interior = supabase.rpc("get_best_cars", {"amount": amount, "interior": True}).execute()
+    result_no_interior = supabase.rpc("get_best_cars", {"amount": amount, "interior": False}).execute()
+    return result_interior.data, result_no_interior.data
+
+
+
+
+
